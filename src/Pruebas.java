18e1e1b38650050008ee3ca1a722f3a13733f2ce
Alba-Guerrero <56840601+Alba-Guerrero@users.noreply.github.com>
2020-10-18 12:34:57 +0200

    Cambios en el main de Pruebas

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -3,6 +3,10 @@ public class Pruebas {
 
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba de GIT");
+		System.out.println("Esto es una prueba de GIT");
+		
+		System.out.println("Esto es una prueba de GIT");
+		
 		
 
 	}
